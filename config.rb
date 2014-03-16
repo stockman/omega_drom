afba899ced7f1cde7608a680abd3561fc9c34a4c
Stickman <stocktonmanners@yahoo.ca>
2014-03-16 10:35:50 -0600

    1
    corrected rb file and added some basic colors

--- config.rb
+++ config.rb
@@ -34,7 +34,7 @@ require 'sass-globbing'
 
 # You can select your preferred output style here (:expanded, :nested, :compact
 # or :compressed).
-output_style = (environment == :production) ? :expanded : :nested
+output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Since Drupal
 # themes can be installed in multiple locations, we don't need to worry about
@@ -42,10 +42,10 @@ output_style = (environment == :production) ? :expanded : :nested
 relative_assets = true
 
 # Conditionally enable line comments when in development mode.
-line_comments = (environment == :production) ? false : true
+line_comments =  true
 
 # Output debugging info in development mode.
-sass_options = (environment == :production) ? {} : {:debug_info => true}
+sass_options =  {:debug_info => true}
 
 # Add the 'sass' directory itself as an import path to ease imports.
 add_import_path 'sass'
